0d3bdc0b6796fe97b27a0aeb7e373bf026e15d9b
Justin <debrabant@cs.brown.edu>
2011-11-21 10:10:23 -0500

    updated cplex tester

--- CplexTester.java
+++ CplexTester.java
@@ -52,12 +52,33 @@ public class CplexTester
 			{
 				out.write(" + " + "x" + i); 
 			}
-			out.write(" <= " + ((max - min + 2) * EPSILON) + "\n"); 
+			/**
+			 * This and following commented out lines can be used to
+			 * write the linear program in "standard" form. 
+			 * Notice that there isn't such a commented line for the
+			 * objective function, because apparently CPLEX does not
+			 * allow to specify constants in the objective function.
+			 * This means that, when using the standard form, one has
+			 * to *subtract* (EPSILON * \sum_{i \in [m,M]} i) from the
+			 * solution given by CPLEX in order to obtain the true
+			 * value of the optimized objective function. Matteo
+			 */
+			//out.write(" <= " + ((max - min + 2) * EPSILON) + "\n"); 
+			out.write(" <= " + EPSILON + "\n"); 
+
+			out.write("c2: " + "x" + min);
+			for (int i = min+1; i <= max; i++)
+			{
+			  	out.write(" + " + "x" + i);
+			}
+			//out.write(" >= " + ((max - min) * EPSILON) + "\n"); 
+			out.write(" >= -" + EPSILON + "\n"); 
 			
 			out.write("Bounds\n"); 
 			for(int i = min; i <= max; i++)
 			{
-				out.write("0 <= x" + i + " <= " + (2 * EPSILON) + "\n"); 
+				//out.write("0 <= x" + i + " <= " + (2 * EPSILON) + "\n"); 
+				out.write("-" + EPSILON + " <= x" + i + " <= " +  EPSILON + "\n"); 
 			}
 			out.write("END"); 
 			

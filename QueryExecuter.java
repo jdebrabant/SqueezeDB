78063921dba5e65d70be16df10498ac2ed7c4eaf
Justin <debrabant@cs.brown.edu>
2011-12-06 15:17:59 -0500

    debugging

--- QueryExecuter.java
+++ QueryExecuter.java
@@ -69,7 +69,7 @@ public class QueryExecuter
 				
 				if(result != null)
 				{
-					processResultSumSampled(result, out); 
+					//processResultSumSampled(result, out); 
 					result.close(); 
 				}
 				else 

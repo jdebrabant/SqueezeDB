2f5da9753720ca96da57dae9bf03b86abc845246
Justin <debrabant@cs.brown.edu>
2011-12-06 07:42:08 -0500

    added QueryExecuter

---
+++ QueryExecuter.java
@@ -0,0 +1,101 @@
+/***************************************************************************************************
+ * File: QueryExecuter.java
+ * Authors: Justin A. DeBrabant (debrabant@cs.brown.edu)
+ * Description: 
+ ***************************************************************************************************/ 
+
+
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
+import java.sql.*; 
+
+public class QueryExecuter
+{
+	
+	public static void main(String [] args)
+	{
+		BufferedReader in; 
+		BufferedWriter out; 
+
+		String query; 
+		
+		Connection conn; 
+		Statement stmt;
+		ResultSet result; 
+		
+		Vector<String> queries = new Vector<String>(); 
+		
+		if(args.length != 5)
+		{
+			System.out.println("Usage: <user> <password> <db> <query file> <result file>"); 
+			System.exit(1); 
+		}
+		
+		try 
+		{
+			conn = DBConnect(args[0], args[1], args[2]);
+			stmt = conn.createStatement(); 
+			
+			in = new BufferedReader(new FileReader(args[3])); 
+			out = new BufferedWriter(new FileWriter(args[4])); 
+			
+			// read in queries from file
+			while((query = in.readLine()) != null)
+			{
+				queries.add(query); 
+			}
+			
+			for(int i = 0; i < queries.size(); i++)
+			{
+				result = stmt.executeQuery(queries.get(i)); 
+				
+				processResult(result, out); 
+			}
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage()); 
+		}
+		
+		
+	}
+	
+	public static void processResult(ResultSet result, BufferedWriter result_out)
+	{
+		try 
+		{
+			while(result.next())
+			{
+				
+			}
+		}
+		catch(Exception e)
+		{
+		}
+	}
+	
+	public static Connection DBConnect(String user, String password, String db)
+	{
+		Connection c = null; 
+
+		String url = new String("jdbc:mysql:");
+		
+		url += db; 
+		
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();  // load the driver
+			c = DriverManager.getConnection(url, user, password);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage()); 
+		}
+		
+		return c; 
+	}
+
+}
---
+++ queries.sql
@@ -0,0 +1,10 @@
+SELECT * FROM table1 WHERE ( ( c_1 <= 495 ));
+SELECT * FROM table1 WHERE ( ( c_1 <= 824 ));
+SELECT * FROM table1 WHERE ( ( c_1 >= 888 ));
+SELECT * FROM table1 WHERE ( ( c_1 <= 430 ));
+SELECT * FROM table1 WHERE ( ( c_1 >= 30 ));
+SELECT * FROM table1 WHERE ( ( c_1 <= 790 ));
+SELECT * FROM table1 WHERE ( ( c_1 <= 228 ));
+SELECT * FROM table1 WHERE ( ( c_1 >= 395 ));
+SELECT * FROM table1 WHERE ( ( c_1 >= 545 ));
+SELECT * FROM table1 WHERE ( ( c_1 >= 793 ));
